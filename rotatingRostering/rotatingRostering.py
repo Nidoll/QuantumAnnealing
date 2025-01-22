b50a788c0ae86efcdd8a1fbfa603a0685a6aa45a
Nidoll <tristan@ase-pv-anlagen.de>
2025-01-22 11:55:43 +0100

    Updated Qubo modell for rotating rostering

--- rotatingRostering/rotatingRostering.py
+++ rotatingRostering/rotatingRostering.py
@@ -7,57 +7,35 @@ from dwave.samplers import SimulatedAnnealingSampler
 import dimod
 import pickle
 import itertools as it
+import json
 
 np.set_printoptions(edgeitems=30,linewidth=10000000000)
 
-n = 5
-days = 5
-shifts = 3
-smin = 1
-smax = 3
+class rotatingRosteringC:
+    def __init__(self, n=5, days=5, shifts=3, smin=1, smax=3, shiftTable=[],offset=0):
+        self.n = n
+        self.days = days
+        self.shifts = shifts 
+        self.smin = smin 
+        self.smax = smax
+        self.offset = offset
 
-# 7 days
-shiftTable7 = np.array([[2,2,2,2,2,4,4],
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4, sort_keys=True)
+    
+    def fromJSON(self, data):
+        self.__dict__ = data
+
+n = 8
+days = 7
+shifts = 4
+smin = 2
+smax = 4
+
+shiftTable = np.array([[2,2,2,2,2,4,4],
                         [2,2,2,2,2,2,2],
                         [2,2,2,2,2,1,1],
                         [2,2,2,2,2,1,1]])
-
-# 3 days
-shiftTable3 = np.array([[2,2,4],
-                        [2,2,2],
-                        [2,2,1],
-                        [2,2,1]])
-
-# 4 days, 4 employe
-shiftTable4small = np.array([[0,0,1,1],
-                             [1,1,1,1],
-                             [2,2,1,1],
-                             [1,1,1,1]])
-
-# 5 days, 4 employe
-shiftTable5small = np.array([[0,0,0,1,1],
-                             [1,1,1,1,1],
-                             [2,2,2,1,1],
-                             [1,1,1,1,1]])
-
-# 5 days, 5 employe, 2 shifts
-shiftTableOther = np.array([[1,1,2,2,2],
-                            [2,2,2,1,1],
-                            [2,2,1,2,2]])
-
-if days == 7:
-    shiftTable = shiftTable7
-elif days == 3:
-    if n == 8:
-        shiftTable = shiftTable3
-elif days == 4:
-    shiftTable = shiftTable4small 
-elif days == 5:
-    if n == 4:
-        shiftTable = shiftTable5small
-        
-if shifts==3:
-    shiftTable = shiftTableOther
 
 vars = Array.create('v', shape=(n,days,shifts), vartype="BINARY")
 H = 0*vars[0,0,0]
@@ -76,28 +54,48 @@ for i in range(days):
 def valToInd(x):
     return (x//days,x%days)
 
-if(smin > 1):
-    #"""
-    # minimum
-    for i in range(n*days):
-        (a,b) = valToInd(i)
-        (c,d) = valToInd((i+1)%(n*days))
-        (e,f) = valToInd((i-1)%(n*days))
-        for j in range(shifts):
-            H += vars[a,b,j] - vars[e,f,j]*vars[a,b,j] - vars[a,b,j]*vars[c,d,j] + vars[e,f,j]*vars[c,d,j]
-    #"""
+
+# minimum
+for i in range(n*days):
+    (a,b) = valToInd(i)
+    (c,d) = valToInd((i+1)%(n*days))
+    (e,f) = valToInd((i-1)%(n*days))
+    for j in range(shifts):
+        xi = vars[a,b,j]
+        xim = vars[e,f,j]
+        xip = vars[c,d,j]
+        q = xi - xim*xi + xim*xip - xi*xip
+
+        H += q
 
 # maximum
 for i in range(n*days):
-    ind = np.zeros(smax+1,dtype=tuple)
+    indList = []
     for j in range(smax+1):
-        ind[j] = valToInd((i+j)%(n*days))
+        indList.append(valToInd((i+j)%(n*days)))
     for j in range(shifts):
-        vlist = []
-        for (a,b) in ind:
-            vlist.append(vars[a,b,j])
-        for (v1,v2) in list(it.product(vlist[:-1],[vlist[-1]])):
-            H += 0.25*v1*v2
+        (x1,y1) = indList[0]
+        xi = vars[x1,y1,j]
+        (x2,y2) = indList[1]
+        xi1 = vars[x2,y2,j]
+        (x3,y3) = indList[2]
+        xi2 = vars[x3,y3,j]
+        (x4,y4) = indList[3]
+        xi3 = vars[x4,y4,j]
+        (x5,y5) = indList[4]
+        xi4 = vars[x5,y5,j]
+        a1 = Binary("a"+str(i)+str(j)+"1")
+        a2 = Binary("a"+str(i)+str(j)+"2")
+        a3 = Binary("a"+str(i)+str(j)+"3")
+        z1 = Binary("z"+str(i)+str(j)+"1")
+        z2 = Binary("z"+str(i)+str(j)+"2")
+        z3 = Binary("z"+str(i)+str(j)+"3")
+        q1 = (xi + xi1 - 2*z1 - a1)**2
+        q2 = (xi2 + xi3 -2*z2 - a2)**2
+        q3 = (z1 + z2 - 2*z3 - a3)**2
+        q4 = (z3*x4)
+        H += (q1+q2+q3+q4)
+            
 
 # weekend same shift
 for i in range(n):
@@ -106,32 +104,47 @@ for i in range(n):
 
 # shift order
 for i in range(n*days):
-    (a,b) = valToInd(i%(n*days))
+    (a,b) = valToInd(i)
     (c,d) = valToInd((i+1)%(n*days))
-    for j in range(shifts):
-        if j == shifts-1:
-            H += vars[a,b,j] - vars[c,d,j]*vars[a,b,j] - vars[a,b,j]*vars[c,d,0] + vars[c,d,j]*vars[c,d,0]
-        elif j > 0:
-            H += (vars[a,b,j] - vars[c,d,j]*vars[a,b,j] - vars[a,b,j]*vars[c,d,0] - vars[a,b,j]*vars[c,d,j+1] 
-                + vars[c,d,j]*vars[c,d,0] + vars[c,d,j+1]*vars[c,d,0] + vars[c,d,j]*vars[c,d,j+1])
 
+    # shift 3
+    x0 = vars[a,b,3]
+    x1 = vars[c,d,0]
+    x2 = vars[c,d,3]
+    H += (x0 - x0*x1 - x0*x2 + 2*x1*x2)
+
+    # shift 2
+    x0 = vars[a,b,2]
+    x1 = vars[c,d,0]
+    x2 = vars[c,d,2]
+    x3 = vars[c,d,3]
+    H += (x0 - x0*x1 - x0*x2 - x0*x3 + 2*x1*x2 + 2*x1*x3 + 2*x2*x3)
         
 # enough free days
-if True:
-    for i in range(n*days):
-        indList = []
-        varList = []
-        for j in range(2*days):
-            indList.append(valToInd((i+j)%(n*days)))
-        for (a,b) in indList:
-            H += (-0.07143*vars[a,b,0])
-            varList.append(vars[a,b,0])
-    
+for i in range(n*days):
+    indList = []
+    varList = []
+    slackls = []
+    for j in range(2*days):
+        indList.append(valToInd((i+j)%(n*days)))
+    for (a,b) in indList:
+        varList.append(vars[a,b,0])
+    s1 = Binary("s"+str(i)+"1")
+    s2 = Binary("s"+str(i)+"2")
+    s3 = Binary("s"+str(i)+"3")
+    s4 = Binary("s"+str(i)+"4")
+    #for j in range(2*days-2):
+        #slackls.append(Binary("s"+str(i)+str(j)))
+    #H += (sum(slackls)-sum(varList)+2)**2
+    H += (1*s1 + 2*s2 + 4*s3 + 8*s4 - sum(varList) + 2)**2
 
 model = H.compile()
 qubo, offset = model.to_qubo()
 bqm = model.to_bqm()
 ising = model.to_ising()
+
+print("Qubo offset:")
+print(offset)
 
 with open("rotatingRosteringQubo.pkl", "wb+") as f:
     pickle.dump(qubo, f)
@@ -141,3 +154,8 @@ with open("rotatingRosteringBqm.pkl", "wb+") as f:
 
 with open("rotatingRosteringIsing.pkl", "wb+") as f:
     pickle.dump(ising, f)
+
+x = rotatingRosteringC(n=n,days=days,shifts=shifts,smin=smin,smax=smax,shiftTable=shiftTable,offset=offset)
+
+with open("rotatingRosteringPara.json", 'w+') as f:
+    f.write(x.toJSON())
